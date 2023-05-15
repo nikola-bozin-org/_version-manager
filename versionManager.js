18b7e8b87c9f8daffea3f4836db3059c421dc254
nikola-bozin-org <nikola.bozin.org@gmail.com>
2023-05-15 12:46:32 +0200

    /

--- versionManager.js
+++ versionManager.js
@@ -4,6 +4,7 @@ const path = require('path');
 const filePath = path.join(__dirname, 'version.json');
 
 const incrementVersion = ()=> {
+  console.info('hello runner!')
   fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
       console.error('Error reading version.json:', err);
